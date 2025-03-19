c8852c59b5294e9e6574472e06431f2bb54303d0
Kuly14 <kulman.david@gmail.com>
2025-03-19 18:29:56 +0100

    impl main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use raft::Config;
+
+const NUM_OF_NODES:u32 = 3;
+
+#[tokio::main]
+async fn main() -> Result<()> { 
+    // TODO: Parse cli args for the id
+    let id = 1;
+    let config = Config::new(id, NUM_OF_NODES);
+    raft::run(config).await?;
+    Ok(())
 }
